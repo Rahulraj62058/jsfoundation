eb220b06d1d2c357e8639fe30aeac92af82f7d37
rdxrahul62 <rahulrajmaurya62058@gmail.com>
2025-11-17 22:20:53 +0530

    nonprimitive data type

---
+++ part2/nonprimitive.js
@@ -0,0 +1,23 @@
+const  usarname= {
+    firstname: "Rahulraj",
+    isLoggedin: true,
+};
+
+// usarname.firstname ="Mr. Rahul"
+// usarname.lastname = "Raj"
+
+// console.log(usarname ["firstname"]);
+// console.log(usarname.lastname);
+// console.log(usarname);
+// console.log(typeof usarname);
+
+let today = new Date();
+// console. log (today.getData());
+
+
+// Array
+
+// let annnotherUser = ["Rahul", true];
+
+let isValue = "2abc";
+console.log(Number(isValue));
